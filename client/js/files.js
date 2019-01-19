9a23494c7b280140b2909d1a517d09de3092b2a8
khanguslee <khanguslee@gmail.com>
2019-01-19 17:11:48 +1100

    Download files from webpage

--- client/js/files.js
+++ client/js/files.js
@@ -8,6 +8,7 @@ fetch('/files')
         console.log(result);
         let files = result.files;
         for(let index=0; index<files.length; index++){
-            $("#list-files").append("<li class='list-group-item'>" + files[index] + "</li>");
+                $("#list-files")
+                .append("<a class='list-group-item list-group-item-action' href='/files/" + files[index] + "'>" + files[index] + "</a>");
         }
     })
