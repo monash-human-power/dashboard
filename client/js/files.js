6d2f5b5bebf06506267e3a93187f26fb171dc1ec
khanguslee <khanguslee@gmail.com>
2019-01-21 01:07:53 +1100

    Delete files from server

--- client/js/files.js
+++ client/js/files.js
@@ -1,3 +1,18 @@
+function deleteFile(e) {
+    $("#deleteModal").modal('show');
+    $("#modal-text").text("Are you sure you want to delete " + e.parentNode.textContent.slice(0,-6) + "?");
+    $("#modalDeleteButton").click(() => {
+        let fileEndpoint = e.parentNode.toString();
+        // Delete file from server
+        fetch(fileEndpoint, { method: "DELETE"});
+        // TODO: Alert to show that you've delete something?
+        $("#deleteModal").modal('hide');
+        e.parentNode.remove();
+    })
+    event.stopPropagation();
+    event.preventDefault();
+}
+
 function loadFiles(){
     // Ask server for list of files
     fetch('/files')
@@ -6,11 +21,11 @@ function loadFiles(){
     })
     .then((result) => {
         // Display list of files
-        console.log(result);
         let files = result.files;
         for(let index=0; index<files.length; index++){
-                $("#list-files")
-                .append("<a class='list-group-item list-group-item-action' href='/files/" + files[index] + "'>" + files[index] + "</a>");
+            $("<a class='list-group-item list-group-item-action' href='/files/" + files[index] + "'>" + files[index] + "</a>")
+                .appendTo($("#list-files"))
+                .append("<span class='btn btn-danger btn-sm float-right delete-button' onclick='deleteFile(this)'>Delete</span")
         }
     })
 }
