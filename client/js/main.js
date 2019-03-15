9a6163eedfdaf5e665703dd07cef965c838f3dbe
rileyclarke <rpclarke99@gmail.com>
2019-03-15 15:51:15 +1100

    Making changes to graphs
    Implemented all recommended changes, apart from file name change.
    
    html element names have been changed, variable created for document elements and chart options to simplify js file. Axis labels have been added and the graph layout has been changed (graphs on right are set to align to the right).

--- client/js/main.js
+++ client/js/main.js
@@ -1,53 +1,49 @@
-document.getElementById("velocity-time").width = document.getElementById("chart1").offsetWidth;
-document.getElementById("velocity-time").height = document.getElementById("chart1").offsetHeight;
+let velocityTimeChart = document.getElementById("velocityTimeChart");
+let velocityChartDiv= document.getElementById("velocityChartDiv");
+let powerTimeChart = document.getElementById("powerTimeChart");
+let powerChartDiv= document.getElementById("powerChartDiv");
+let cadenceTimeChart = document.getElementById("cadenceTimeChart");
+let cadenceChartDiv= document.getElementById("cadenceChartDiv");
 
-new Chart(document.getElementById("velocity-time"), {
-                type:'scatter',
-                data:{
-                    label:'Scatter Dataset',
-                    datasets:[{
-                    data:[{x:0, y:0},{x:5, y:2},{x:10, y:22},{x:15, y:40},{x:20, y:56},{x:25, y:70},{x:30, y:82},{x:35, y:92},{x:40, y:100}],
-                    backgroundColor:['rgba(165, 105, 189, 0.6)']
-                    }]
-                    },
+cadenceTimeChart.width = cadenceChartDiv.offsetWidth;
+cadenceTimeChart.width = cadenceChartDiv.offsetHeight;
+powerTimeChart.width = powerChartDiv.offsetWidth;
+powerTimeChart.width = powerChartDiv.offsetHeight;
+velocityTimeChart.width = velocityChartDiv.offsetWidth;
+velocityTimeChart.width = velocityChartDiv.offsetHeight;
 
-                options:{
-                    title:{
-                        display:true,
-                        text:'Velocity-Time',
-                        maintainAspectRatio: true,
-                        fontSize:14
-                    },
-                    
-                    legend: {
-                        display: false
-                    },
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true,
-                                max: 175
-                                }
-                            }]
-                        }
-              }
-            });
-
-
-document.getElementById("power-time").width = document.getElementById("chart2").offsetWidth;
-document.getElementById("power-time").height = document.getElementById("chart2").offsetHeight;
-
-new Chart(document.getElementById("power-time"), {
-                type:'scatter',
-                data:{
-                    label:'Scatter Dataset',
-                    datasets:[{
-                    data:[{x:0, y:0},{x:5, y:12},{x:10, y:53},{x:15, y:25},{x:20, y:46},{x:25, y:28},{x:30, y:18},{x:35, y:89},{x:40, y:153}],
-                    backgroundColor:['rgba(93, 173, 226, 0.6)']
-                    }]
-                    },
-
-                options:{
+var velocityTimeOptions = {
+        title:{
+        display:true,
+        text:'Velocity-Time',
+        maintainAspectRatio: true,
+        fontSize:14
+        },        
+        legend: {
+            display: false
+        },
+        scales: {
+            xAxes: [{
+                display: true,
+                scaleLabel: {
+                  display: true,
+                  labelString: 'Time (s)'
+                }
+              }],
+            yAxes: [{
+                display: true,
+                scaleLabel: {
+                  display: true,
+                  labelString: 'Velocity (km/h)'
+                },
+                ticks: {
+                    beginAtZero: true,
+                    max: 175
+                    }
+                }]
+            }
+        };
+var powerTimeOptions = {
                     title:{
                         display:true,
                         text:'Power-Time',
@@ -59,30 +55,27 @@ new Chart(document.getElementById("power-time"), {
                         display: false
                     },
                     scales: {
+                        xAxes: [{
+                            display: true,
+                            scaleLabel: {
+                              display: true,
+                              labelString: 'Time (s)'
+                            }
+                          }],
                         yAxes: [{
+                            display: true,
+                            scaleLabel: {
+                              display: true,
+                              labelString: 'Power (W)'
+                            },
                             ticks: {
                                 beginAtZero: true,
                                 max: 175
                                 }
                             }]
                         }
-              }
-            });
-
-document.getElementById("cadence-time").width = document.getElementById("chart3").offsetWidth;
-document.getElementById("cadence-time").height = document.getElementById("chart3").offsetHeight;
-
-new Chart(document.getElementById("cadence-time"), {
-                type:'scatter',
-                data:{
-                    label:'Scatter Dataset',
-                    datasets:[{
-                    data:[{x:0, y:10},{x:5, y:45},{x:10, y:53},{x:15, y:25},{x:20, y:85},{x:25, y:53},{x:30, y:47},{x:35, y:13},{x:40, y:43}],
-                    backgroundColor:['rgba(88, 214, 141, 0.6)']
-                    }]
-                    },
-
-                options:{
+              };
+var cadenceTimeOptions = {
                     title:{
                         display:true,
                         text:'Cadence-Time',
@@ -94,14 +87,62 @@ new Chart(document.getElementById("cadence-time"), {
                         display: false
                     },
                     scales: {
+                        xAxes: [{
+                            display: true,
+                            scaleLabel: {
+                              display: true,
+                              labelString: 'Time (s)'
+                            }
+                          }],
                         yAxes: [{
+                            display: true,
+                            scaleLabel: {
+                              display: true,
+                              labelString: 'Cadence'
+                            },
                             ticks: {
                                 beginAtZero: true,
                                 max: 175
                                 }
                             }]
                         }
-              }
+              };
+
+
+new Chart(document.getElementById("velocityTimeChart"), {
+                type:'scatter',
+                data:{
+                    label:'Scatter Dataset',
+                    datasets:[{
+                        data:[{x:0, y:0},{x:5, y:2},{x:10, y:22},{x:15, y:40},{x:20, y:56},{x:25, y:70},{x:30, y:82},{x:35, y:92},{x:40, y:100}],
+                        backgroundColor:['rgba(165, 105, 189, 0.6)']
+                        }]
+                    },
+                options: velocityTimeOptions
+            });
+
+new Chart(document.getElementById("powerTimeChart"), {
+                type:'scatter',
+                data:{
+                    label:'Scatter Dataset',
+                    datasets:[{
+                        data:[{x:0, y:0},{x:5, y:12},{x:10, y:53},{x:15, y:25},{x:20, y:46},{x:25, y:28},{x:30, y:18},{x:35, y:89},{x:40, y:153}],
+                        backgroundColor:['rgba(93, 173, 226, 0.6)']
+                        }]
+                    },
+                options: powerTimeOptions
+            });
+
+new Chart(document.getElementById("cadenceTimeChart"), {
+                type:'scatter',
+                data:{
+                    label:'Scatter Dataset',
+                    datasets:[{
+                        data:[{x:0, y:10},{x:5, y:45},{x:10, y:53},{x:15, y:25},{x:20, y:85},{x:25, y:53},{x:30, y:47},{x:35, y:13},{x:40, y:43}],
+                        backgroundColor:['rgba(88, 214, 141, 0.6)']
+                        }]
+                    },
+                options: cadenceTimeOptions
             });
 
 
