ba2940f6be6422311b67e0a8d8c0d28c5f8f7560
Kuan Hou Lee <angus.lee809@gmail.com>
2019-03-15 21:28:33 +1100

    Install chart.js via npm and use grunt to include the package

--- client/js/index.js
+++ client/js/index.js
@@ -115,7 +115,8 @@ new Chart(velocityTimeChart, {
                     label:'Scatter Dataset',
                     datasets:[{
                         data:[{x:0, y:0},{x:5, y:2},{x:10, y:22},{x:15, y:40},{x:20, y:56},{x:25, y:70},{x:30, y:82},{x:35, y:92},{x:40, y:100}],
-                        backgroundColor:['rgba(165, 105, 189, 0.6)']
+                        backgroundColor:['rgba(165, 105, 189, 0.6)'],
+                        showLine: true
                         }]
                     },
                 options: velocityTimeOptions
@@ -127,7 +128,8 @@ new Chart(powerTimeChart, {
                     label:'Scatter Dataset',
                     datasets:[{
                         data:[{x:0, y:0},{x:5, y:12},{x:10, y:53},{x:15, y:25},{x:20, y:46},{x:25, y:28},{x:30, y:18},{x:35, y:89},{x:40, y:153}],
-                        backgroundColor:['rgba(93, 173, 226, 0.6)']
+                        backgroundColor:['rgba(93, 173, 226, 0.6)'],
+                        showLine: true
                         }]
                     },
                 options: powerTimeOptions
@@ -139,7 +141,8 @@ new Chart(cadenceTimeChart, {
                     label:'Scatter Dataset',
                     datasets:[{
                         data:[{x:0, y:10},{x:5, y:45},{x:10, y:53},{x:15, y:25},{x:20, y:85},{x:25, y:53},{x:30, y:47},{x:35, y:13},{x:40, y:43}],
-                        backgroundColor:['rgba(88, 214, 141, 0.6)']
+                        backgroundColor:['rgba(88, 214, 141, 0.6)'],
+                        showLine: true
                         }]
                     },
                 options: cadenceTimeOptions
