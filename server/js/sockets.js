224a8152b78fd531fdcd91173abd7767615eeebf
khanguslee <angus.lee809@gmail.com>
2019-03-28 17:41:37 +1100

    Client to average data point every second

--- server/js/sockets.js
+++ server/js/sockets.js
@@ -4,15 +4,21 @@
 let sockets = {};
 
 function mqttConnected() {
-    console.log("Connected to mqtt broker");
+    console.log('Connected to mqtt broker');
 }
 
 function mqttDataTopicHandler(socket, payload) {
+    // Parse data
     let message ={};
     let dataArray = payload.split('&');
     for (let index = 0; index < dataArray.length; index++) {
         data = dataArray[index].split('=');
-        message[data[0]] = data[1];
+        // No need to average filename value
+        if (data[0] === 'filename') {
+            message[data[0]] = data[1];
+        } else {
+            message[data[0]] = Number(data[1]);
+        }
     }
     socket.emit('data', message);
 }
