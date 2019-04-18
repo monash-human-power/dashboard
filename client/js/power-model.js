491b2520b5d6ae1e212c9d2dc40601894f8e28cc
khanguslee <angus.lee809@gmail.com>
2019-04-18 17:24:03 +1000

    Notify users that user has started/stopped power model

--- client/js/power-model.js
+++ client/js/power-model.js
@@ -5,12 +5,17 @@ const socket = io();
 function startPowerModel() {
     console.log('Starting power model');
     socket.emit('start-power-model');
+    document.getElementById('start-button').disabled = true;
+    document.getElementById('status-success').style.display = 'block';
+    document.getElementById('status-success-message').innerHTML = 'Started power model!';
 }
 
 function stopPowerModel() {
     console.log('Stopping power model');
     socket.emit('stop-power-model');
     document.getElementById('start-button').disabled = false;
+    document.getElementById('status-success').style.display = 'block';
+    document.getElementById('status-success-message').innerHTML = 'Stopping power model!';
 }
 
 socket.on('power-model-running', function() {
