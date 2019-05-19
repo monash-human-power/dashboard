d0668a32a211c7efed58dabd5c04e1341e68e2f8
Kuan Hou Lee <angus.lee809@gmail.com>
2019-05-19 16:47:30 +1000

    Fix lint errors

--- client/js/power-calibration.js
+++ client/js/power-calibration.js
@@ -1,21 +1,22 @@
+/* global io */
 const socket = io();
-
+// eslint-disable-next-line no-unused-vars
 function resetDistance() {
   console.log('Reset distance');
   socket.emit('reset-calibration');
 }
 
+// eslint-disable-next-line no-unused-vars
 function submitActualDistance() {
-  const calibrated_distance = document.getElementById('input-calibration')
-    .value;
-  if (calibrated_distance < 0 || calibrated_distance == '') {
+  const calibratedDistance = document.getElementById('input-calibration').value;
+  if (calibratedDistance < 0 || calibratedDistance === '') {
     console.error('Invalid distance');
     document.getElementById('invalid-distance-message').style.display = 'block';
     document.getElementById('valid-distance-message').style.display = 'none';
     return;
   }
-  console.log(`Submit calibration distance: ${calibrated_distance}`);
-  socket.emit('submit-calibration', calibrated_distance);
+  console.log(`Submit calibration distance: ${calibratedDistance}`);
+  socket.emit('submit-calibration', calibratedDistance);
   document.getElementById('input-calibration').value = '';
   document.getElementById('invalid-distance-message').style.display = 'none';
   document.getElementById('valid-distance-message').style.display = 'block';
