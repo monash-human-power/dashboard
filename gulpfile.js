62fe7a098c954686d60af7ad6e6fcc81a22eb07a
khanguslee <angus.lee809@gmail.com>
2019-08-01 12:37:32 +1000

    Add missing js file

--- gulpfile.js
+++ gulpfile.js
@@ -15,9 +15,11 @@ const bootstrapJsPath =
   'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 const jqueryJsPath = 'node_modules/jquery/dist/jquery.min.js';
 const chartJsPath = 'node_modules/chart.js/dist/Chart.bundle.min.js';
+const fontAwesomeJsPath =
+  'node_modules/@fortawesome/fontawesome-free/js/regular.min.js';
 gulp.task('js', () => {
   return gulp
-    .src([bootstrapJsPath, jqueryJsPath, chartJsPath])
+    .src([bootstrapJsPath, jqueryJsPath, chartJsPath, fontAwesomeJsPath])
     .pipe(gulp.dest('client/js'));
 });
 
