63b1f395240e88023679a972b46c8d519c64eb21
Angus Trau <me@angus.ws>
2019-12-22 20:57:38 +1100

    Create download files page

--- dashboard/src/router/index.js
+++ dashboard/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Dashboard from '@/views/Dashboard.vue';
+import DownloadFiles from '@/views/DownloadFiles.vue';
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'dashboard',
     component: Dashboard,
+  },
+  {
+    path: '/download-files',
+    name: 'download-files',
+    component: DownloadFiles,
   },
 ];
 
