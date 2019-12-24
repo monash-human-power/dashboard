c0dca3ced250ddad6e35839f7fc6a98103eabf7f
Angus Trau <me@angus.ws>
2019-12-24 12:37:44 +1100

    Scope styles

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -20,7 +20,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .app-body {
   display: flex;
   flex-direction: row;
--- dashboard/src/components/Sidebar.vue
+++ dashboard/src/components/Sidebar.vue
@@ -34,6 +34,6 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 
 </style>
