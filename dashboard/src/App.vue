35e9bd5dcc75fdc387f6d2cf3fa16780824c26a3
Angus Trau <angustrau@users.noreply.github.com>
2019-12-25 18:41:54 +1100

    Create Vue project structure and implement navigation (#40)
    * Generate vue project
    
    * Add bootstrap
    
    * Add nav and sidebar
    
    * Refactor sidebar
    
    * Change title
    
    * Remove example code
    
    * Proxy to backend server
    
    * Add socket.io dependency
    
    * Install chartjs
    
    * Rename Home view to Dashboard
    
    * Change relative to absolute path
    
    * Update README
    
    * Add component names
    
    * Scope styles
    
    * Change component names

---
+++ dashboard/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="app">
+    <div>
+      <MainHeader />
+    </div>
+    <div class="app-body">
+      <MainSidebar />
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import MainHeader from '@/components/MainHeader.vue';
+import MainSidebar from '@/components/MainSidebar.vue';
+
+export default {
+  name: 'app',
+  components: { MainHeader, MainSidebar },
+};
+</script>
+
+<style scoped>
+.app-body {
+  display: flex;
+  flex-direction: row;
+}
+</style>
---
+++ dashboard/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import DashboardView from '@/views/DashboardView.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'dashboard-view',
+    component: DashboardView,
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
