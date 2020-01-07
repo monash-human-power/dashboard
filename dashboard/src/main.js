5300006c9c32a11d82b6074d76328ac2f0bc860a
Angus Trau <me@angus.ws>
2020-01-07 20:00:09 +1100

    Add vuex

--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -1,15 +1,16 @@
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
-import App from './App.vue';
-import router from './router';
-
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app');
