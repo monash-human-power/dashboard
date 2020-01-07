7d38ace9d9f283db8ae9be41ffce99fde164da31
Angus Trau <me@angus.ws>
2020-01-07 21:27:26 +1100

    Proxy socket.io connection

--- dashboard/vue.config.js
+++ dashboard/vue.config.js
@@ -4,6 +4,10 @@ module.exports = {
       '^/(files|server/status)': {
         target: 'http://localhost:5000',
       },
+      '^/socket.io': {
+        target: 'ws://localhost:5000',
+        ws: true,
+      },
     },
   },
 };
