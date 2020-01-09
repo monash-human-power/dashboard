2b615ffe5fcb43754bd9b243865354e6b431ec13
Angus Trau <me@angus.ws>
2020-01-09 17:24:39 +1100

    Pass through props on WidgetListItem

--- client/src/components/WidgetListItem.js
+++ client/src/components/WidgetListItem.js
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 import { ListGroupItem } from 'react-bootstrap';
 import styles from './WidgetListItem.module.css';
 
-export default function WidgetListItem({ title, children }) {
+export default function WidgetListItem({ title, children, ...props }) {
   return (
     <>
-      <ListGroupItem className={styles.item}>
+      <ListGroupItem className={styles.item} {...props}>
         {title}
         {children}
       </ListGroupItem>
