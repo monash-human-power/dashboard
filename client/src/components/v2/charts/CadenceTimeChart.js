36fa58bdaac9a98fdaa134adb750dc7d7fa6b079
Angus Trau <me@angus.ws>
2020-01-11 19:30:55 +1100

    Add cadence-time chart

---
+++ client/src/components/v2/charts/CadenceTimeChart.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Scatter } from 'react-chartjs-2';
+import AnnotationPlugin from 'chartjs-plugin-annotation';
+import { useSensorTimeSeries } from 'api/v2/sensors';
+
+export default function CadenceTimeChart({ interval }) {
+  const { series, max } = useSensorTimeSeries('cadence', interval);
+
+  const options = {
+    title: {
+      display: true,
+      text: 'Cadence-Time',
+      maintainAspectRatio: true,
+      fontSize: 14,
+    },
+
+    legend: {
+      display: false,
+    },
+    scales: {
+      xAxes: [
+        {
+          display: true,
+          scaleLabel: {
+            display: true,
+            labelString: 'Time (s)',
+          },
+        },
+      ],
+      yAxes: [
+        {
+          display: true,
+          scaleLabel: {
+            display: true,
+            labelString: 'Cadence',
+          },
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+    annotation: {
+      drawTime: 'afterDraw',
+      annotations: [
+        {
+          type: 'line',
+          mode: 'horizontal',
+          scaleID: 'y-axis-1',
+          value: max,
+          borderColor: '#57606f',
+          borderDash: [10, 10],
+          label: {
+            enabled: true,
+            content: `${max} RPM`,
+            yAdjust: 15,
+          },
+        },
+      ],
+    },
+    plugins: [AnnotationPlugin],
+  };
+
+  const data = {
+    label: 'Scatter Dataset',
+    datasets: [
+      {
+        data: series.map((point) => ({
+          x: point.time / 1000,
+          y: point.value,
+        })),
+        backgroundColor: ['rgba(88, 214, 141, 0.6)'],
+        showLine: true,
+      },
+    ],
+  };
+
+  return (
+    <Scatter
+      options={options}
+      data={data}
+    />
+  );
+}
+
+CadenceTimeChart.propTypes = {
+  interval: PropTypes.number.isRequired,
+};
--- client/src/views/DashboardView.js
+++ client/src/views/DashboardView.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import CadenceTimeChart from 'components/v2/charts/CadenceTimeChart';
 
 export default function DashboardView() {
+  const updateInterval = 1000;
+
   return (
-    <div>Dashboard</div>
+    <Container>
+      <CadenceTimeChart interval={updateInterval} />
+    </Container>
   );
 }
