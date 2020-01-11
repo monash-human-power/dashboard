4f34c1ca7a806630fa6ce1b7a5f432ece4906a0e
Angus Trau <me@angus.ws>
2020-01-12 02:46:10 +1100

    Add text mode toggle

--- client/src/views/DashboardView.js
+++ client/src/views/DashboardView.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import React, { useState, useCallback } from 'react';
+import {
+  Col,
+  Container,
+  FormCheck,
+  Row,
+} from 'react-bootstrap';
 import VelocityTimeChart from 'components/v2/charts/VelocityTimeChart';
 import PowerTimeChart from 'components/v2/charts/PowerTimeChart';
 import CadenceTimeChart from 'components/v2/charts/CadenceTimeChart';
+import { useData } from 'api/v2/sensors';
 
 /**
  * Dashboard page component
@@ -10,23 +16,50 @@ import CadenceTimeChart from 'components/v2/charts/CadenceTimeChart';
  * @returns {React.Component} Component
  */
 export default function DashboardView() {
+  const [textMode, setTextMode] = useState(false);
+  const data = useData();
   const updateInterval = 1000;
+
+  const handleTextMode = useCallback((event) => {
+    setTextMode(event.currentTarget.checked);
+  }, []);
 
   return (
     <Container fluid>
       <Row>
-        <Col lg={6}>
-          <VelocityTimeChart interval={updateInterval} />
+        <Col sm={8}>
+          <h5 style={{ visibility: !data ? 'hidden' : 'visible' }}>
+            {`Current Filename: ${data?.filename}`}
+          </h5>
         </Col>
-        <Col lg={6}>
-          <PowerTimeChart interval={updateInterval} />
+        <Col sm={4}>
+          <FormCheck
+            type="switch"
+            label="Text mode"
+            id="text-mode-switch"
+            className="float-right"
+            value={textMode}
+            onChange={handleTextMode}
+          />
         </Col>
       </Row>
-      <Row>
-        <Col lg={6}>
-          <CadenceTimeChart interval={updateInterval} />
-        </Col>
-      </Row>
+      {!textMode ? (
+        <div>
+          <Row>
+            <Col lg={6}>
+              <VelocityTimeChart interval={updateInterval} />
+            </Col>
+            <Col lg={6}>
+              <PowerTimeChart interval={updateInterval} />
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={6}>
+              <CadenceTimeChart interval={updateInterval} />
+            </Col>
+          </Row>
+        </div>
+      ) : null}
     </Container>
   );
 }
