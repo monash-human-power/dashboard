a774987e7932dcc39b8ffcee62b84ff660a12402
Angus Trau <me@angus.ws>
2020-01-12 20:22:12 +1100

    Add power model data api

--- client/src/api/v2/powerModel.js
+++ client/src/api/v2/powerModel.js
@@ -2,6 +2,49 @@ import { useState, useCallback } from 'react';
 import { emit, useChannel } from './socket';
 
 /**
+ * @typedef {object} PowerModelData
+ * @property {number} rec_speed           Recommended Speed
+ * @property {number} rec_power           Recommended Power
+ * @property {number} zdist               Zone Distance Left
+ * @property {number} distance_offset     Distance Offset
+ * @property {number} distance_left       Total Distance Left
+ * @property {number} predicted_max_speed Predicted Max Speed
+ */
+
+/**
+ * Use current power model data
+ *
+ * @returns {?PowerModelData} Power model data
+ */
+export function usePowerModel() {
+  const [recSpData, setRecSpData] = useState({
+    rec_speed: null,
+    rec_power: null,
+    zdist: null,
+    distance_offset: null,
+    distance_left: null,
+  });
+  const [maxSpData, setMaxSpData] = useState({
+    predicted_max_speed: null,
+  });
+
+  const recSpHandler = useCallback((data) => {
+    setRecSpData(data);
+  }, []);
+  useChannel('power-model-recommended-SP', recSpHandler);
+
+  const maxSpHandler = useCallback((data) => {
+    setMaxSpData(data);
+  }, []);
+  useChannel('power-model-max-speed', maxSpHandler);
+
+  return {
+    ...recSpData,
+    ...maxSpData,
+  };
+}
+
+/**
  * @typedef {object} PowerModelStateHook
  * @property {boolean}            running     Is the power model running
  * @property {function(boolean)}  setRunning  Set power model running state
