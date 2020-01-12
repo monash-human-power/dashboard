93448df02c32b111c7fd311166789fdd3ecae35e
Angus Trau <me@angus.ws>
2020-01-12 23:38:18 +1100

    Switch dashboard view to sensor data api

--- client/src/views/DashboardView.js
+++ client/src/views/DashboardView.js
@@ -10,7 +10,7 @@ import PowerTimeChart from 'components/v2/charts/PowerTimeChart';
 import CadenceTimeChart from 'components/v2/charts/CadenceTimeChart';
 import LocationTimeChart from 'components/v2/charts/LocationTimeChart';
 import TextModeChart from 'components/v2/charts/TextModeChart';
-import { useData } from 'api/v2/sensors';
+import { useSensorData } from 'api/v2/sensors';
 
 /**
  * Dashboard page component
@@ -19,7 +19,7 @@ import { useData } from 'api/v2/sensors';
  */
 export default function DashboardView() {
   const [textMode, setTextMode] = useState(false);
-  const data = useData();
+  const { data: filename } = useSensorData('filename');
   const updateInterval = 1000;
 
   const handleTextMode = useCallback((event) => {
@@ -30,8 +30,8 @@ export default function DashboardView() {
     <Container fluid>
       <Row className="align-items-center">
         <Col sm={8}>
-          <h5 style={{ visibility: !data ? 'hidden' : 'visible' }}>
-            {`Current Filename: ${data?.filename}`}
+          <h5 style={{ visibility: filename === null ? 'hidden' : 'visible' }}>
+            {`Current Filename: ${filename}`}
           </h5>
         </Col>
         <Col sm={4}>
