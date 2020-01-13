3133e48fd5972bdba4767d1b63094ce89ab80754
Angus Trau <me@angus.ws>
2020-01-13 17:46:39 +1100

    Drop null-loader dependency

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,15 +1,13 @@
 const {
   override,
-  addWebpackModuleRule,
+  addWebpackExternals,
 } = require('customize-cra');
-const path = require('path');
 
 module.exports = override(
-  // Chart.js has a required dependency on moment.js. However this dependency is actually optional.
-  // It is only required when using the Time Cartesion axis.
-  // As we don't use this part of the module, we can nullify moment.js for a 50kB bundle reduction
-  addWebpackModuleRule({
-    test: path.resolve(__dirname, 'node_modules/moment'),
-    loader: 'null-loader',
+  // Chart.js has a required dependency on moment.js. However this dependency is only required when
+  // using the Time Cartesion axis. As we don't use this part of the module, we can drop moment.js
+  // from the bundle for a 50kB bundle reduction.
+  addWebpackExternals({
+    moment: 'moment',
   }),
 );
