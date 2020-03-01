2a2eab5e72d9d3797094e744e0d8d9258d5e4d94
Angus Trau <me@angus.ws>
2020-03-01 14:57:24 +1100

    Move routes to routes/v2

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,59 +1,24 @@
-import DashboardView from 'views/v2/DashboardView';
-import DownloadFilesView from 'views/v2/DownloadFilesView';
-import SensorStatusView from 'views/v2/SensorStatusView';
-import PowerModelView from 'views/v2/PowerModelView';
-import PowerMapView from 'views/v2/PowerMapView';
-import PowerModelCalibrationView from 'views/v2/PowerModelCalibrationView';
-import CameraSettingsView from 'views/v2/CameraSettingsView';
-import OptionsView from 'views/v2/OptionsView';
+import { useRouteMatch } from 'react-router-dom';
+import { routes as V2Routes } from './v2';
+
+const versionRoutes = {
+  v2: V2Routes,
+};
 
 export const routes = [
-  {
-    name: 'Dashboard',
-    path: '/',
-    exact: true,
-    component: DashboardView,
-  },
-  {
-    name: 'Files',
-    path: '/download-files',
-    exact: true,
-    component: DownloadFilesView,
-  },
-  {
-    name: 'Sensors',
-    path: '/status',
-    exact: true,
-    component: SensorStatusView,
-  },
-  {
-    name: 'Power Model',
-    path: '/power-model',
-    exact: true,
-    component: PowerModelView,
-  },
-  {
-    name: 'Power Map',
-    path: '/power-zone',
-    exact: true,
-    component: PowerMapView,
-  },
-  {
-    name: 'Power Model Calibration',
-    path: '/power-calibration',
-    exact: true,
-    component: PowerModelCalibrationView,
-  },
-  {
-    name: 'Camera',
-    path: '/camera',
-    exact: true,
-    component: CameraSettingsView,
-  },
-  {
-    name: 'Options',
-    path: '/options',
-    exact: true,
-    component: OptionsView,
-  },
+  ...V2Routes,
 ];
+
+/**
+ * Get the list of routes for the selected bike version
+ *
+ * @returns {import('./route').RouteInfo[]} Route list
+ */
+export function useRouteList() {
+  const match = useRouteMatch('/:version');
+  const version = match?.params?.version;
+  if (version && versionRoutes[version]) {
+    return versionRoutes[version];
+  }
+  return [];
+}
