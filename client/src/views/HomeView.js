d3b6824adef4c8008433b7e7767013394f53e275
Angus Trau <me@angus.ws>
2020-03-02 14:08:30 +1100

    Change home to a redirect to /v3

--- client/src/views/HomeView.js
+++ client/src/views/HomeView.js
@@ -1,31 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
-import styles from './HomeView.module.css';
+import { Redirect } from 'react-router-dom';
 
 /**
- * Home view and version switcher component
+ * Home view component
  *
  * @returns {React.Component} Component
  */
 export default function HomeView() {
-  return (
-    <div className={styles.home}>
-      <Button
-        size="lg"
-        className="mb-2"
-        as={Link}
-        to="/v2"
-      >
-        Version 2 (Wombat)
-      </Button>
-      <Button
-        size="lg"
-        as={Link}
-        to="/v3"
-      >
-        Version 3 (V3)
-      </Button>
-    </div>
-  );
+  return <Redirect to="/v3" />;
 }
