b33f4ea9121971d41d34242cc91ba8571a4481ae
Angus Trau <me@angus.ws>
2020-03-02 15:25:59 +1100

    Refactor to show current version in navbar

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,9 +2,26 @@ import { useRouteMatch } from 'react-router-dom';
 import HomeView from 'views/HomeView';
 import { routes as V2Routes } from './v2';
 
-const versionRoutes = {
-  v2: V2Routes,
-};
+/**
+ * @typedef {object} VersionInfo
+ * @property {string} name Bike version friendly name
+ * @property {string} rootPath Base path for the version
+ * @property {import('./route').RouteInfo[]} routes List of routes under this version
+ */
+
+/** @type {VersionInfo[]} */
+export const bikeVersions = [
+  {
+    name: 'Version 2 (Wombat)',
+    rootPath: '/v2',
+    routes: V2Routes,
+  },
+  {
+    name: 'Version 3 (V3)',
+    rootPath: '/v3',
+    routes: [],
+  },
+];
 
 export const routes = [
   {
@@ -13,19 +30,22 @@ export const routes = [
     exact: true,
     component: HomeView,
   },
-  ...V2Routes,
 ];
+bikeVersions.forEach((v) => {
+  routes.push(...v.routes);
+});
 
 /**
- * Get the list of routes for the selected bike version
+ * Get the route info for the selected bike version
  *
- * @returns {import('./route').RouteInfo[]} Route list
+ * @returns {VersionInfo} Bike version route info
  */
-export function useVersionRoutes() {
+export function useBikeVersion() {
   const match = useRouteMatch('/:version');
   const version = match?.params?.version;
-  if (version && versionRoutes[version]) {
-    return versionRoutes[version];
+  const bikeVersion = bikeVersions.find((v) => v.rootPath === `/${version}`);
+  if (bikeVersion) {
+    return bikeVersion;
   }
-  return [];
+  return null;
 }
