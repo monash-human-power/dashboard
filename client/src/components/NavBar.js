5928e176dd9356ae614fbfb53343839fac34ee61
Angus Trau <angustrau@users.noreply.github.com>
2020-03-03 05:52:06 +0000

    Refactor to show current version in navbar (#64)

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import { useVersionRoutes } from 'router';
+import { useBikeVersion, bikeVersions } from 'router';
 import { ReactComponent as Logo } from 'assets/MHPLogo.svg';
 import styles from './NavBar.module.css';
 
@@ -11,8 +11,9 @@ import styles from './NavBar.module.css';
  * @returns {React.Component} Component
  */
 export default function NavBar() {
-  const routes = useVersionRoutes();
-  const linkItems = routes.map(({ name, path, exact }) => (
+  const bikeVersion = useBikeVersion();
+  const bikeName = bikeVersion?.name;
+  const linkItems = bikeVersion?.routes.map(({ name, path, exact }) => (
     <Nav.Link
       as={NavLink}
       to={path}
@@ -22,6 +23,16 @@ export default function NavBar() {
     >
       {name}
     </Nav.Link>
+  ));
+
+  const versionDropdown = bikeVersions.map(({ name, rootPath }) => (
+    <NavDropdown.Item
+      as={NavLink}
+      to={rootPath}
+      activeClassName=""
+    >
+      {name}
+    </NavDropdown.Item>
   ));
 
   return (
@@ -40,9 +51,8 @@ export default function NavBar() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav>
-          <NavDropdown title="Version" id="version-selector">
-            <NavDropdown.Item as={NavLink} to="/v2">Version 2 (Wombat)</NavDropdown.Item>
-            <NavDropdown.Item as={NavLink} to="/v3">Version 3 (V3)</NavDropdown.Item>
+          <NavDropdown title={bikeName} id="version-selector">
+            {versionDropdown}
           </NavDropdown>
         </Nav>
         <Nav className="ml-auto">
