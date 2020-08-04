0520b89162423b75dee71176496353c6f5717b5e
haotongwang <50545432+haotongwang@users.noreply.github.com>
2020-08-04 12:45:35 +1000

    Add controls to start and stop camera recording (#74)
    * Add CameraRecording component
    
    Include component in CameraSettingsView
    Add CameraRecording helper functions to api
    
    * Implement mqtt for dashboard
    
    Add socket.io emit from client for starting and stopping recording
    Add socket.io listen for updating status
    Add mqtt publish from server for start and stop
    Subscribe server to /v3/camera/recording/status/<primary, secondary>
    
    * Add status messages to dashboard
    
    Implement different information for each status
    Add util function for representing bytes in a more readable unit
    
    * Generalise CameraRecording with CameraRecordingStatus
    
    CameraRecording is a wrapper for multiple CameraRecordingStatus
    CameraRecordingStatus is the status for a single camera
    Generalise mqtt code for server for catching camera recording statuses
    
    * Add CameraRecordingStatus to API
    
    Use custom react hook for status
    
    * Store last payload to server

--- client/src/api/v2/camera.js
+++ client/src/api/v2/camera.js
@@ -1,4 +1,6 @@
 import { useState, useEffect, useCallback } from 'react';
+import formatBytes from 'utils/formatBytes';
+import { camelCaseToStartCase, capitalise } from 'utils/string';
 import { useChannel, emit } from './socket';
 
 /**
@@ -38,4 +40,120 @@ export function useOverlays(device) {
   }, [device]);
 
   return { overlays, setActiveOverlay };
+}
+
+/**
+ * Send a message to server to start recording video
+ */
+export function startRecording() {
+  emit('start-camera-recording');
+}
+
+/**
+ * Send a message to server to stop recording video
+ */
+export function stopRecording() {
+  emit('stop-camera-recording');
+}
+
+/**
+ * @typedef {object} CameraRecordingStatusPayload
+ * @property {string} status Status of recording
+ * @property {?string} recordingMinutes Length of recording
+ * @property {?string} recordingFile File name of recording
+ * @property {?string} diskSpaceRemaining Remaining space on disk
+ * @property {?string} error Error message from camera
+ */
+
+/**
+ * Parse the payload into an object and format the values
+ *
+ * Payload structure is defined in the 'V3 MQTT Topics' page on Notion.
+ * Topic is /v3/camera/recording/status/<primary/>secondary>.
+ *
+ * @param {string} payload Payload
+ * @returns {CameraRecordingStatusPayload} Formatted payload without status
+ */
+function parsePayload(payload) {
+  const data = JSON.parse(payload);
+  if (!data) return null;
+
+  const formattedData = {};
+
+  Object.keys(data)
+    .forEach((field) => {
+      let name = camelCaseToStartCase(field);
+      let value = '';
+
+      // format field value
+      switch (field) {
+        case 'status':
+          value = capitalise(data[field]);
+          break;
+
+        case 'diskSpaceRemaining':
+          value = formatBytes(data.diskSpaceRemaining);
+          break;
+
+        case 'recordingMinutes':
+          {
+            const mins = Math.floor(data.recordingMinutes);
+            const secs = Math.floor((data.recordingMinutes - mins) * 60);
+            value = `${mins}m ${secs}s`;
+            name = 'Recording Time';
+          }
+          break;
+
+        default:
+          value = data[field];
+          break;
+      }
+
+      formattedData[name] = value;
+    });
+
+  return formattedData;
+}
+
+/**
+ * Initiate receiving camera recording statuses
+ */
+function initCameraStatus() {
+  emit('send-last-received-camera-recording-payloads');
+}
+
+/**
+ * Returns the last status payload published
+ *
+ * @param {string} device Device
+ * @returns {CameraRecordingStatusPayload} Payload
+ */
+export function useCameraRecordingStatus(device) {
+  // only run init once per render
+  useEffect(() => {
+    initCameraStatus();
+  }, []);
+
+  const [lastPayload, setLastPayload] = useState(null);
+
+  const update = useCallback((newPayload) => {
+    // update last payload
+    setLastPayload(newPayload);
+  }, []);
+
+  useChannel(`camera-recording-status-${device}`, update);
+
+  return parsePayload(lastPayload);
+}
+
+/**
+ * Makes the device name pretty.
+ *
+ * Hardcoded for efficiency. If you don't like it, complain to Angus Trau :).
+ *
+ * @param {'primary' | 'secondary'} device Device
+ * @returns {string} Prettied device name
+ */
+export function getPrettyDeviceName(device) {
+  return device === 'primary' ? 'Primary' : 'Secondary';
 }
---
+++ client/src/utils/formatBytes.js
@@ -0,0 +1,16 @@
+/**
+ * Converts number of bytes to a more human readable unit.
+ *
+ * By StackOverflow's community https://stackoverflow.com/questions/15900485/correct-way-to-convert-size-in-bytes-to-kb-mb-gb-in-javascript
+ *
+ * @param {number} bytes -- Number of bytes
+ * @param {number} digits -- Number of decimal places to round to
+ * @returns {string} bytes represented in a format with units
+ */
+export default function formatBytes(bytes, digits = 2) {
+  if (bytes === 0) return '0 Bytes';
+  const units = ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+  const d = digits < 0 ? 0 : digits;
+  const mag = Math.floor(Math.log(bytes) / Math.log(1024));
+  return `${parseFloat((bytes / (1024 ** mag)).toFixed(d))} ${units[mag]}`;
+}
