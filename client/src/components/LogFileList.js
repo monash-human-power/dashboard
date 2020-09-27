039a5a01183f64be2706d2f645e7fb02042a9dca
Christopher Hall <hallgchris@gmail.com>
2020-09-27 12:48:11 +1000

    Document LofFileList

--- client/src/components/LogFileList.js
+++ client/src/components/LogFileList.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import WidgetListGroupItem from 'components/WidgetListGroupItem';
+import LogFile from '../api/v2/files';
 
+/**
+ * @typedef {object} LogFileListProps
+ * @property {LogFile[]} files List of log files to display
+ * @property {(file: string) => void} onDeleteFile Function called when user deletes a log
+ */
+
+/**
+ * List of log files with delete buttons
+ *
+ * @param {LogFileListProps} Props props
+ * @returns {React.Component<LogFileListProps>} Component
+ */
 export default function LogFileList({ files, onDeleteFile }) {
   const handleDelete = (event, file) => {
     event.preventDefault();
