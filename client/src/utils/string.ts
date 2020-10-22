c4ef911fee49be6c3a5aead9c18d6a397998db34
Christopher Hall <hallgchris@gmail.com>
2020-10-23 09:37:54 +1100

    Port string.ts to TS

--- client/src/utils/string.ts
+++ client/src/utils/string.ts
@@ -1,20 +1,20 @@
 /**
  * Capitalise a string
  *
- * @param {string} string String to capitalise
- * @returns {string} Capitalised string
+ * @param string String to capitalise
+ * @returns Capitalised string
  */
-export function capitalise(string) {
+export function capitalise(string: string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 /**
  * Convert a camel cased string ('fooBar') to start case ('Foo Bar')
  *
- * @param {string} string String to convert
- * @returns {string} Start case string
+ * @param string String to convert
+ * @returns Start case string
  */
-export function camelCaseToStartCase(string) {
+export function camelCaseToStartCase(string: string) {
   const split = string.replace(/([A-Z])/g, ' $1');
   return capitalise(split);
 }
@@ -24,11 +24,11 @@ export function camelCaseToStartCase(string) {
  *
  * By StackOverflow's community https://stackoverflow.com/questions/15900485/correct-way-to-convert-size-in-bytes-to-kb-mb-gb-in-javascript
  *
- * @param {number} bytes -- Number of bytes
- * @param {number} digits -- Number of decimal places to round to
- * @returns {string} bytes represented in a format with units
+ * @param bytes -- Number of bytes
+ * @param digits -- Number of decimal places to round to
+ * @returns Bytes represented in a format with units
  */
-export function formatBytes(bytes, digits = 2) {
+export function formatBytes(bytes: number, digits: number = 2) {
   if (bytes === 0) return '0 Bytes';
   const units = [
     'Bytes',
@@ -49,10 +49,10 @@ export function formatBytes(bytes, digits = 2) {
 /**
  * Converts a number of minutes to the format mm:ss.
  * 
- * @param {number} totalMinutes number of minuets to format
- * @returns {string} The formatted duration.
+ * @param totalMinutes Number of minuets to format
+ * @returns The formatted duration.
  */
-export function formatMinutes(totalMinutes) {
+export function formatMinutes(totalMinutes: number) {
   const mins = Math.floor(totalMinutes);
   const secs = Math.floor((totalMinutes - mins) * 60);
   return `${mins}m ${secs}s`;
