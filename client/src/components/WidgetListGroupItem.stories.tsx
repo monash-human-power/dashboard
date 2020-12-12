cf7e12bf82d99239aa2846e98fcd51cc95181958
Christopher Hall <hallgchris@gmail.com>
2020-12-12 20:09:13 +1100

    Use Nick's story constructor magic

--- client/src/components/WidgetListGroupItem.stories.tsx
+++ client/src/components/WidgetListGroupItem.stories.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { addArgs, createStory } from '../utils/stories';
 import WidgetListGroupItem, {
   WidgetListGroupItemProps,
 } from './WidgetListGroupItem';
@@ -12,22 +13,22 @@ export default {
   },
 };
 
-const template = (args: WidgetListGroupItemProps) => () => (
-  <WidgetListGroupItem {...args} />
-);
+const Template = addArgs<WidgetListGroupItemProps>((props) => (
+  <WidgetListGroupItem {...props} />
+));
 
 const baseProps = { title: 'List group item', active: false };
 
-export const Simple = template({ ...baseProps });
+export const Simple = createStory(Template, { ...baseProps });
 
-export const Active = template({ ...baseProps, active: true });
+export const Active = createStory(Template, { ...baseProps, active: true });
 
-export const ButtonWidget = template({
+export const ButtonWidget = createStory(Template, {
   ...baseProps,
   children: <Button size="sm">Button</Button>,
 });
 
-export const Link = template({
+export const Link = createStory(Template, {
   ...baseProps,
   action: true,
   href: 'https://www.monashhumanpower.org',
---
+++ client/src/utils/stories.ts
@@ -0,0 +1,10 @@
+export type StoryTemplate<T> = ((a: T) => JSX.Element) & { args: T };
+
+export const addArgs = <T>(f: (a: T) => JSX.Element): StoryTemplate<T> =>
+  Object.assign(f, { args: {} as T });
+
+export const createStory = <T>(templateFunc: StoryTemplate<T>, args: T) => {
+  const story = templateFunc.bind({});
+  story.args = args;
+  return story;
+};
