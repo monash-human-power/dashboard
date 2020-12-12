7494542ebf7f3e569a24795204752c4bae9b1b26
Nick Wang <nicholas.wang.h@gmail.com>
2020-12-12 21:41:45 +1100

    Indent using 2 spaces

--- client/src/components/v2/CameraStatus.tsx
+++ client/src/components/v2/CameraStatus.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Badge, Card, Col, Row } from 'react-bootstrap';
 
 export interface CameraStatusProps {
-    /** Status of primary device */
-    primaryStatus: boolean
-    /** Status of secondary device */
-    secondaryStatus: boolean
+  /** Status of primary device */
+  primaryStatus: boolean
+  /** Status of secondary device */
+  secondaryStatus: boolean
 }
 
 /**
@@ -19,29 +19,29 @@ export interface CameraStatusProps {
  * @returns Component
  */
 export default function CameraStatus({ primaryStatus, secondaryStatus }: CameraStatusProps) {
-    return (
-        <Card>
-            <Card.Body>
-                <Card.Title>Camera Status</Card.Title>
-                <Card.Subtitle>
-                    <Row>
-                        <Col>
-                            <Badge pill variant={primaryStatus ? 'success' : 'danger'}>
-                                {primaryStatus ? 'Connected' : 'Disconnected'}
-                            </Badge>
-                            <br />
-                            <span>Primary Camera</span>
-                        </Col>
-                        <Col>
-                            <Badge pill variant={secondaryStatus ? 'success' : 'danger'}>
-                                {secondaryStatus ? 'Connected' : 'Disconnected'}
-                            </Badge>
-                            <br />
-                            <span>Secondary Camera</span>
-                        </Col>
-                    </Row>
-                </Card.Subtitle>
-            </Card.Body>
-        </Card>
-    );
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>Camera Status</Card.Title>
+        <Card.Subtitle>
+          <Row>
+            <Col>
+              <Badge pill variant={primaryStatus ? 'success' : 'danger'}>
+                {primaryStatus ? 'Connected' : 'Disconnected'}
+              </Badge>
+              <br />
+              <span>Primary Camera</span>
+            </Col>
+            <Col>
+              <Badge pill variant={secondaryStatus ? 'success' : 'danger'}>
+                {secondaryStatus ? 'Connected' : 'Disconnected'}
+              </Badge>
+              <br />
+              <span>Secondary Camera</span>
+            </Col>
+          </Row>
+        </Card.Subtitle>
+      </Card.Body>
+    </Card>
+  );
 }
