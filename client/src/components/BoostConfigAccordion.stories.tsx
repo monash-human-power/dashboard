3e268ecd9c72b37f3a63d289a91879979e06841d
Christopher Hall <hallgchris@gmail.com>
2020-12-19 12:36:08 +1100

    Add stories for no selection and no options

--- client/src/components/BoostConfigAccordion.stories.tsx
+++ client/src/components/BoostConfigAccordion.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { addArgs, createStory } from 'utils/stories';
+import { BoostConfig } from 'types/boost';
 import BoostConfigAccordion, {
   BoostConfigAccordionProps,
 } from './BoostConfigAccordion';
@@ -14,32 +15,52 @@ const Template = addArgs<BoostConfigAccordionProps>((props) => (
   <BoostConfigAccordion {...props} />
 ));
 
+const baseConfigs: BoostConfig[] = [
+  {
+    type: 'powerPlan',
+    options: ['my_plan_1.json', 'this_one_gets_you_to_144.json'],
+    active: 'my_plan_1.json',
+  },
+  {
+    type: 'rider',
+    options: ['al.json', 'charles.json'],
+    active: 'charles.json',
+  },
+  {
+    type: 'bike',
+    options: ['blacksmith.json', 'wombat.json', 'precilla.json'],
+    active: 'wombat.json',
+  },
+  {
+    type: 'track',
+    options: ['ford.json', 'holden.json', 'battle_mountain.json'],
+    active: 'ford.json',
+  },
+];
+
 const onSelectConfig = action('onSelectConfig');
 const onDeleteConfig = action('onDeleteConfig');
 
 export const Simple = createStory(Template, {
-  configs: [
-    {
-      type: 'powerPlan',
-      options: ['my_plan_1.json', 'this_one_gets_you_to_144.json'],
-      active: 'my_plan_1.json',
-    },
-    {
-      type: 'rider',
-      options: ['al.json', 'charles.json'],
-      active: 'charles.json',
-    },
-    {
-      type: 'bike',
-      options: ['blacksmith.json', 'wombat.json', 'precilla.json'],
-      active: 'wombat.json',
-    },
-    {
-      type: 'track',
-      options: ['ford.json', 'holden.json', 'battle_mountain.json'],
-      active: 'ford.json',
-    },
-  ],
+  configs: baseConfigs,
+  onSelectConfig,
+  onDeleteConfig,
+});
+
+export const NoSelection = createStory(Template, {
+  configs: baseConfigs.map((config) =>
+    config.type === 'rider'
+      ? { type: config.type, options: config.options }
+      : config,
+  ),
+  onSelectConfig,
+  onDeleteConfig,
+});
+
+export const NoOptions = createStory(Template, {
+  configs: baseConfigs.map((config) =>
+    config.type === 'rider' ? { type: config.type, options: [] } : config,
+  ),
   onSelectConfig,
   onDeleteConfig,
 });
