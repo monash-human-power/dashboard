0f94332866aeb5fe6474d78cc510ae020a6b2150
Christopher Hall <hallgchris@gmail.com>
2020-12-19 21:45:25 +1100

    Add remaining visual card things

--- client/src/components/BoostConfigurator.tsx
+++ client/src/components/BoostConfigurator.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Accordion, Card } from 'react-bootstrap';
+import { Accordion, Button, Card } from 'react-bootstrap';
 import { BoostConfig, BoostConfigType } from 'types/boost';
 import { camelCaseToStartCase } from 'utils/string';
 import BoostConfigList from './BoostConfigList';
@@ -16,29 +16,37 @@ export default function BoostConfigurator({
   onDeleteConfig,
 }: BoostConfiguratorProps) {
   return (
-    <Accordion>
-      {configs.map((config, index) => (
-        <Card>
-          <Accordion.Toggle
-            as={Card.Header}
-            variant="link"
-            eventKey={String(index)}
-          >
-            {camelCaseToStartCase(config.type)}
-            {': '}
-            <i>{config.active ? `"${config.active}"` : 'None'}</i>
-          </Accordion.Toggle>
-          <Accordion.Collapse eventKey={String(index)}>
-            <Card.Body>
-              <BoostConfigList
-                config={config}
-                onSelectConfig={(name) => onSelectConfig(config.type, name)}
-                onDeleteConfig={(name) => onDeleteConfig(config.type, name)}
-              />
-            </Card.Body>
-          </Accordion.Collapse>
-        </Card>
-      ))}
-    </Accordion>
+    <Card>
+      <Card.Body>
+        <Card.Title>Configuration</Card.Title>
+        <Button variant="outline-primary" className="mb-3" onClick={() => {}}>
+          Upload config
+        </Button>
+        <Accordion>
+          {configs.map((config, index) => (
+            <Card>
+              <Accordion.Toggle
+                as={Card.Header}
+                variant="link"
+                eventKey={String(index)}
+              >
+                {camelCaseToStartCase(config.type)}
+                {': '}
+                <i>{config.active ? `"${config.active}"` : 'None'}</i>
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey={String(index)}>
+                <Card.Body>
+                  <BoostConfigList
+                    config={config}
+                    onSelectConfig={(name) => onSelectConfig(config.type, name)}
+                    onDeleteConfig={(name) => onDeleteConfig(config.type, name)}
+                  />
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          ))}
+        </Accordion>
+      </Card.Body>
+    </Card>
   );
 }
