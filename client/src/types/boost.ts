9ebc8c9cf61933f9813677668d93708f9dfc3f82
Christopher Hall <hallgchris@gmail.com>
2020-12-29 16:32:39 +1100

    Add BOOST config selecter component/stories (#93)
    * Add story for active WidgetListGroupItem
    
    * Port WidgetListGroupItem and stories to TS
    
    * Use Component Story Format for WidgetListGroupItem
    
    * Remove hopefully redundant control
    
    * Use Nick's story constructor magic
    
    * Add BoostConfigList with a simple story
    
    * Use font awesome trash for delete button
    
    * Handle when no configs are uploaded
    
    * Add basic BoostConfigAccordion
    
    * Generalise BoostConfigAccordion props
    
    * Move BOOST types to types directory
    
    * Refactor BoostConfigList to accept a BoostConfig
    
    * Show active config name in accordion
    
    * Add stories for no selection and no options
    
    * Rename BoostConfigAccordion to BoostConfigurator
    
    * Add remaining visual card things
    
    * Add callback for uploading config
    
    * Tidy up ommission of properties in stories

---
+++ client/src/types/boost.ts
@@ -0,0 +1,10 @@
+export type BoostConfigType = 'powerPlan' | 'rider' | 'bike' | 'track';
+
+export interface BoostConfig {
+  /** The input of BOOST that this config is for */
+  type: BoostConfigType;
+  /** List of available BOOST configuration files */
+  options: string[];
+  /** Currently selected BOOST configuration */
+  active?: string;
+}
