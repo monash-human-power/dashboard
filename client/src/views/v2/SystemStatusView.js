2b15d3b1cf38ba9a2aa9325be46e4cf8f663a820
Blake Haydon <blake@haydon.dev>
2021-01-22 14:46:48 +1100

    Add system status page

---
+++ client/src/views/v2/SystemStatusView.js
@@ -0,0 +1,28 @@
+import { useStatus } from 'api/v2/sensors';
+import ContentPage from 'components/ContentPage';
+import WidgetListGroupItem from 'components/WidgetListGroupItem';
+import React from 'react';
+import { Badge, ListGroup } from 'react-bootstrap';
+
+/**
+ * Sensor Status page component
+ *
+ * @returns {React.Component} Component
+ */
+export default function SensorStatusView() {
+    const sensorStatus = useStatus();
+
+    const sensorItems = sensorStatus.map(({ label, name, state }) => (
+        <WidgetListGroupItem title={label} key={name}>
+            <Badge pill variant={state ? 'success' : 'danger'}>
+                {state ? 'ON' : 'OFF'}
+            </Badge>
+        </WidgetListGroupItem>
+    ));
+
+    return (
+        <ContentPage title="Sensor Status">
+            <ListGroup>{sensorItems}</ListGroup>
+        </ContentPage>
+    );
+}
