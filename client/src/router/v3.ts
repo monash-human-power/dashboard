7997fddf7ac673197a1d36a443b225ee5d379675
Blake Haydon <blake@haydon.dev>
2021-01-28 10:58:55 +1100

    Create v3 routes file

---
+++ client/src/router/v3.ts
@@ -0,0 +1,63 @@
+import CameraSettingsView from 'views/v2/CameraSettingsView';
+import DashboardView from 'views/v2/DashboardView';
+import DownloadFilesView from 'views/v2/DownloadFilesView';
+import OptionsView from 'views/v2/OptionsView';
+import PowerMapView from 'views/v2/PowerMapView';
+import PowerModelCalibrationView from 'views/v2/PowerModelCalibrationView';
+import PowerModelView from 'views/v2/PowerModelView';
+import SensorStatusView from 'views/v2/SensorStatusView';
+import { RouteInfo } from './route';
+
+/**
+ * V3 views
+ */
+export const routes: RouteInfo[] = [
+  {
+    name: 'Dashboard',
+    path: '/v2/',
+    exact: true,
+    component: DashboardView as unknown as React.Component,
+  },
+  {
+    name: 'Files',
+    path: '/v2/download-files',
+    exact: true,
+    component: DownloadFilesView as unknown as React.Component,
+  },
+  {
+    name: 'Sensors',
+    path: '/v2/status',
+    exact: true,
+    component: SensorStatusView as unknown as React.Component,
+  },
+  {
+    name: 'Power Model',
+    path: '/v2/power-model',
+    exact: true,
+    component: PowerModelView as unknown as React.Component,
+  },
+  {
+    name: 'Power Map',
+    path: '/v2/power-zone',
+    exact: true,
+    component: PowerMapView as unknown as React.Component,
+  },
+  {
+    name: 'Power Model Calibration',
+    path: '/v2/power-calibration',
+    exact: true,
+    component: PowerModelCalibrationView as unknown as React.Component,
+  },
+  {
+    name: 'Camera',
+    path: '/v2/camera',
+    exact: true,
+    component: CameraSettingsView as unknown as React.Component,
+  },
+  {
+    name: 'Options',
+    path: '/v2/options',
+    exact: true,
+    component: OptionsView as unknown as React.Component,
+  },
+];
