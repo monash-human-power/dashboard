9706ae0040fc40c2211c31e91db0df830a8a4cb0
Kunj Dave <kdav0006@student.monash.edu>
2021-02-13 20:35:30 +1100

    Make BoostView pass in props for BoostCalibration

--- client/src/views/common/BoostView.tsx
+++ client/src/views/common/BoostView.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ContentPage from 'components/common/ContentPage';
 import BoostCalibration from 'components/common/boost/BoostCalibration';
-
+import {setCalibration, resetCalibration} from 'api/common/powerModel';
 
 /**
  * Boost View component
@@ -11,7 +11,7 @@ import BoostCalibration from 'components/common/boost/BoostCalibration';
 export default function BoostView() {
     return (
         <ContentPage title="Boost Configuration">
-            <BoostCalibration/>
+            <BoostCalibration onSet={setCalibration} onReset={resetCalibration}/>
         </ContentPage>
     );
 };
