9343c1b8f0e1cea4fa48bebad15af36bedab8e80
Kunj Dave <kdav0006@student.monash.edu>
2021-02-21 22:23:33 +1100

    Add docs

--- client/src/api/v3/boost.ts
+++ client/src/api/v3/boost.ts
@@ -1,10 +1,17 @@
 import { emit } from 'api/common/socket';
 import { BoostConfigType } from 'types/boost';
 
+/**
+ * Sends the content of the given configuration file on 'boost/configs/action' over MQTT
+ * 
+ * @param configType the type of the configuration being sent
+ * @param configFiles list of files, only the first would be considered
+ */
 export default function uploadConfig(configType: BoostConfigType, configFiles: FileList) {
     const topic = 'boost/configs/action';
     const reader = new FileReader();
     
+    // Called when FileReader has completed reading a file
     reader.onload = () => {
         console.log(reader.result);
         const payload = {
@@ -12,7 +19,7 @@ export default function uploadConfig(configType: BoostConfigType, configFiles: F
             "configType": configType,
             "content": reader.result,
         };
-        emit(topic, JSON.stringify(payload));
+        emit(topic, JSON.stringify(payload).replace(/\\n/g, ''));
     };
 
     reader.readAsText(configFiles[0]);
