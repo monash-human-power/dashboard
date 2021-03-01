f435c3b79c5bfe25f29cb30b1f2392d84435e9dd
Kunj Dave <kdav0006@student.monash.edu>
2021-03-01 21:44:42 +1100

    Extract sending payload functionality into a different function

--- client/src/api/v3/boost.ts
+++ client/src/api/v3/boost.ts
@@ -1,6 +1,27 @@
 import { emit } from 'api/common/socket';
 import { BoostConfigType } from 'types/boost';
 
+type action = 'upload' | 'delete';
+
+/**
+ * Send configuration status over MQTT on topic 'boost/configs/action'
+ * 
+ * @param actionType actionType represents whether the config is being uploaded or deleted
+ * @param type type the type of the configuration being sent
+ * @param configContent configuration content
+ */
+function sendConfig(actionType: action, type: BoostConfigType, configContent: string | null) {
+  const topic = 'send-config';
+  const payload = {
+    action: actionType,
+    configType: type,
+    content: configContent,
+  };
+  // To check things work without needing the server started
+  console.log(payload);
+  emit(topic, JSON.stringify(payload));
+}
+
 /**
  * Send the content of the given configuration file on `boost/configs/action` over MQTT
  *
@@ -11,18 +32,13 @@ export default function uploadConfig(
   type: BoostConfigType,
   configFile: File,
 ) {
-  const topic = 'send-config';
   const reader = new FileReader();
 
   // Called when FileReader has completed reading a file
   reader.onload = () => {
-    console.log(reader.result);
-    const payload = {
-      action: 'upload',
-      configType: type,
-      content: reader.result,
-    };
-    emit(topic, JSON.stringify(payload));
+    if (typeof reader.result === 'string' ) {
+    sendConfig('upload', type, reader.result);
+    }
   };
 
   reader.readAsText(configFile);
