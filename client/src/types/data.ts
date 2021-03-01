1fe35040e9738a1bb395f6217a654dc45af2a23f
haotongwang <50545432+haotongwang@users.noreply.github.com>
2021-03-01 15:23:13 +1100

    Add API functions for accessing wireless module data (#103)
    * Add wireless module channel to server
    
    * Extract into variables
    
    * Add hook for module data
    
    * Fix method name
    
    * Change parameter type to number
    
    * Use array deconstruction assignment
    
    * Remove T from type
    
    * Update topics.js
    
    * Add types for data sensors
    
    * Add useSensorData
    
    * Increase number of listeners 11 -> 20
    
    * Indent long line
    
    * Add pdop and datetime to gpsrt
    
    * Add comment for null stuff

---
+++ client/src/types/data.ts
@@ -0,0 +1,112 @@
+import { Number, Record, Static, String } from "runtypes";
+
+/* ------------------------------------ RunTypes ------------------------------------ */
+
+/** Value runtype of temperature sensor data */
+export const TemperatureRT = Number;
+
+/** Value runtype of humidity sensor data */
+export const HumidityRT = Number;
+
+/** Value runtype of steeringAngle sensor data */
+export const SteeringAngleRT = Number;
+
+/** Value runtype of co2 sensor data */
+export const CO2RT = Number;
+
+/** Value runtype of accelerometer sensor data */
+export const AccelerometerRT = Record({
+    /** X value */
+    x: Number,
+    /** Y value */
+    y: Number,
+    /** Z value */
+    z: Number
+});
+
+/** Value runtype of gyroscope sensor data */
+export const GyroscopeRT = Record({
+    /** X value */
+    x: Number,
+    /** Y value */
+    y: Number,
+    /** Z value */
+    z: Number
+});
+
+/** Value runtype of reedVelocity sensor data */
+export const ReedVelocityRT = Number;
+
+/** Value runtype of reedDistance sensor data */
+export const ReedDistanceRT = Number;
+
+/** Value runtype of gps sensor data */
+export const GPSRT = Record({
+    /** Speed */
+    speed: Number,
+    /** Dilution of precision */
+    pdop: Number,
+    /** Latitude */
+    latitude: Number,
+    /** Longitude */
+    longitude: Number,
+    /** Altitude */
+    altitude: Number,
+    /** Course */
+    course: Number,
+    /** Datetime */
+    datetime: String
+});
+
+/** Value runtype of power sensor data */
+export const PowerRT = Number;
+
+/** Value runtype of cadence sensor data */
+export const CadenceRT = Number;
+
+/** Value runtype of heartRate sensor data */
+export const HeartRateRT = Number;
+
+/* ------------------------------------ Types ------------------------------------ */
+
+/** Value type of temperature sensor data */
+export type TemperatureT = Static<typeof TemperatureRT>
+
+/** Value type of humidity sensor data */
+export type HumidityT = Static<typeof HumidityRT>
+
+/** Value type of steeringAngle sensor data */
+export type SteeringAngleT = Static<typeof SteeringAngleRT>
+
+/** Value type of co2 sensor data */
+export type CO2T = Static<typeof CO2RT>
+
+/** Value type of accelerometer sensor data */
+export type AccelerometerT = Static<typeof AccelerometerRT>
+
+/** Value type of gyroscope sensor data */
+export type GyroscopeT = Static<typeof GyroscopeRT>
+
+/** Value type of reedVelocity sensor data */
+export type ReedVelocityT = Static<typeof ReedVelocityRT>
+
+/** Value type of reedDistance sensor data */
+export type ReedDistanceT = Static<typeof ReedDistanceRT>
+
+/** Value type of gps sensor data */
+export type GPST = Static<typeof GPSRT>
+
+/** Value type of power sensor data */
+export type PowerT = Static<typeof PowerRT>
+
+/** Value type of cadence sensor data */
+export type CadenceT = Static<typeof CadenceRT>
+
+/** Value type of heartRate sensor data */
+export type HeartRateT = Static<typeof HeartRateRT>
+
+/** Union type of all sensor value types */
+export type SensorsT = TemperatureT | HumidityT | SteeringAngleT
+    | CO2T | AccelerometerT | GyroscopeT
+    | ReedVelocityT | ReedDistanceT | GPST
+    | PowerT | CadenceT | HeartRateT;
