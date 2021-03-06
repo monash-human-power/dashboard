fedcf6600699e918b1bf77cbf72a54c8fc841bb9
Nick Wang <nicholas.wang.h@gmail.com>
2021-03-06 18:16:46 +1100

    hook up api to stats

--- client/src/components/v3/StatisticRow.tsx
+++ client/src/components/v3/StatisticRow.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import { Sensor, useSensorData } from 'api/common/data';
+import React, { useEffect, useState } from 'react';
+import { HeartRateRT, PowerRT, ReedVelocityRT } from 'types/data';
 import Statistic from './Statistic';
 import styles from './StatisticRow.module.css';
 
@@ -8,28 +10,50 @@ import styles from './StatisticRow.module.css';
  * @returns Component
  */
 export default function StatisticRow(): JSX.Element {
-    const { value } = { value: 501 };
+    // Should only be positive, abs max of currSpeed
+    const [maxSpeed, setMaxSpeed] = useState<number | null>(null);
+
+    // TODO: Multiple sensor data
+    const currSpeed = useSensorData(3, Sensor.ReedVelocity, ReedVelocityRT);
+
+    // Update max speed whenever the speed is updated
+    useEffect(() => {
+        // Add new data point
+        if (currSpeed) setMaxSpeed(Math.max(currSpeed, Math.abs(maxSpeed ?? 0)));
+    }, [currSpeed, maxSpeed]);
+
+    // TODO: Trap locations
+    // eslint-disable-next-line no-unused-vars
+    const [prevTrapSpeed, setPrevTrapSpeed] = useState(null);
+
+    // TODO: Predicted trap speed
+    // eslint-disable-next-line no-unused-vars
+    const [nextTrapSpeed, setNextTrapSpeed] = useState(null);
+
+    const power = useSensorData(4, Sensor.Power, PowerRT);
+
+    const heartRate = useSensorData(4, Sensor.HeartRate, HeartRateRT);
 
     return (
         <div className={styles.statContainer}>
             <div className={styles.stat}>
-                <Statistic value={value} unit="km/h" desc="Current speed" />
+                <Statistic value={currSpeed} unit="km/h" desc="Current speed" />
             </div>
             <div className={styles.stat}>
-                <Statistic value={value} unit="km/h" desc="Current speed" />
+                <Statistic value={maxSpeed} unit="km/h" desc="Max. speed" />
             </div>
             <div className={styles.stat}>
-                <Statistic value={value} unit="km/h" desc="Current speed" />
+                <Statistic value={prevTrapSpeed} unit="km/h" desc="Prev. trap speed (archived" />
             </div>
             <div className={styles.stat}>
-                <Statistic value={value} unit="km/h" desc="Current speed" />
+                <Statistic value={nextTrapSpeed} unit="km/h" desc="Next trap speed (predicted)" />
             </div>
             <div className={styles.line} />
             <div className={styles.stat}>
-                <Statistic value={value} unit="km/h" desc="Current speed" />
+                <Statistic value={power ? Math.round(power) : null} unit="W" desc="Power" />
             </div>
             <div className={styles.stat}>
-                <Statistic value={value} unit="km/h" desc="Current speed" />
+                <Statistic value={heartRate ? Math.round(heartRate) : null} unit="bpm" desc="Heart rate" />
             </div>
         </div>
     );
