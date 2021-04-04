3c57d9e6f5467de2cc38a26ce577beabeb1c0ccd
Nick Wang <nicholas.wang.h@gmail.com>
2021-04-04 20:31:09 +1000

    prettier

--- client/src/components/common/OnlineStatusPill.stories.tsx
+++ client/src/components/common/OnlineStatusPill.stories.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { addArgs, createStory } from 'utils/stories';
-import OnlineStatusPill, { OnlineStatusPillProps } from 'components/common/OnlineStatusPill';
+import OnlineStatusPill, {
+  OnlineStatusPillProps,
+} from 'components/common/OnlineStatusPill';
 
 export default {
-    component: OnlineStatusPill,
-    title: 'components/common/OnlineStatusPill'
+  component: OnlineStatusPill,
+  title: 'components/common/OnlineStatusPill',
 };
 
 // Stories Template
-const Template = addArgs<OnlineStatusPillProps>(
-    props => <OnlineStatusPill {...props} />
-);
+const Template = addArgs<OnlineStatusPillProps>((props) => (
+  <OnlineStatusPill {...props} />
+));
 
 // Stories
 export const Connected = createStory(Template, { isOnline: true });
--- client/src/components/common/OnlineStatusPill.tsx
+++ client/src/components/common/OnlineStatusPill.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Badge } from 'react-bootstrap';
 
-export type isOnlineT = boolean | null
+export type isOnlineT = boolean | null;
 
 /**
  * @property {boolean | null} isOnline Whether the status is on, off or error
  */
 export interface OnlineStatusPillProps {
-    /** Specify whether the status is on, off or error */
-    isOnline: isOnlineT;
+  /** Specify whether the status is on, off or error */
+  isOnline: isOnlineT;
 }
 
 /**
@@ -17,10 +17,30 @@ export interface OnlineStatusPillProps {
  * @returns Component
  */
 export default function OnlineStatusPill({ isOnline }: OnlineStatusPillProps) {
-    switch (isOnline) {
-        case true: return (<Badge pill variant='success'> Online </Badge>);
-        case false: return (<Badge pill variant='secondary'> Offline </Badge>);
-        case null: return (<Badge pill variant='danger'> ERROR </Badge>);
-        default: return (<Badge pill variant='danger'> ERROR </Badge>);
-    }
+  switch (isOnline) {
+    case true:
+      return (
+        <Badge pill variant="success">
+          Online
+        </Badge>
+      );
+    case false:
+      return (
+        <Badge pill variant="secondary">
+          Offline
+        </Badge>
+      );
+    case null:
+      return (
+        <Badge pill variant="danger">
+          ERROR
+        </Badge>
+      );
+    default:
+      return (
+        <Badge pill variant="danger">
+          ERROR
+        </Badge>
+      );
+  }
 }
