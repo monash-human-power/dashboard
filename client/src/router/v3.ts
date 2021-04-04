9bef82f5ec3a7a465089a63c92a383c3ecb5d18c
Christopher Hall <hallgchris@gmail.com>
2021-04-04 10:22:03 +1000

    Use common CameraSystemView on v3 dashboard

--- client/src/router/v3.ts
+++ client/src/router/v3.ts
@@ -2,7 +2,7 @@ import DashboardView from 'views/v3/DashboardView';
 import LogsView from 'views/common/LogsView';
 import StatusView from 'views/v3/StatusView';
 import BoostView from 'views/common/BoostView';
-import CameraSystemView from 'views/v3/CameraSystemView';
+import CameraSystemView from 'views/common/CameraSystemView';
 import { RouteInfo } from 'router/route';
 
 /**
@@ -13,31 +13,30 @@ export const routes: RouteInfo[] = [
     name: 'Dashboard',
     path: '/v3/',
     exact: true,
-    component: DashboardView as unknown as React.Component,
+    component: (DashboardView as unknown) as React.Component,
   },
   {
     name: 'Logs',
     path: '/v3/logs',
     exact: true,
-    component: LogsView as unknown as React.Component,
+    component: (LogsView as unknown) as React.Component,
   },
   {
     name: 'Status',
     path: '/v3/status',
     exact: true,
-    component: StatusView as unknown as React.Component,
+    component: (StatusView as unknown) as React.Component,
   },
   {
     name: 'Boost',
     path: '/v3/boost',
     exact: true,
-    component: BoostView as unknown as React.Component,
+    component: (BoostView as unknown) as React.Component,
   },
   {
     name: 'Camera System',
     path: '/v3/camera-system',
     exact: true,
-    component: CameraSystemView as unknown as React.Component,
-  }
+    component: (CameraSystemView as unknown) as React.Component,
+  },
 ];
-
