effa5ab5e1c5c8ee765027e1d77153258f682d3f
Eric <eyem0002@student.monsah.edu>
2022-02-07 23:15:10 +1100

    added confirmation modal

--- client/src/components/v3/DASRecording.tsx
+++ client/src/components/v3/DASRecording.tsx
@@ -8,9 +8,8 @@ import {
   useModuleStopCallback,
 } from 'api/common/data';
 import React, { useState } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
 import toast from 'react-hot-toast';
-
 /**
  * Turn recording off and on
  *
@@ -18,6 +17,9 @@ import toast from 'react-hot-toast';
  */
 export default function DASRecording(): JSX.Element {
   const [loggingEnabled, setLoggingEnabled] = useState(false);
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
+  const closeConfirmModal = () => setShowConfirmModal(false);
+  const openConfirmModal = () => setShowConfirmModal(true);
 
   useModuleStartCallback(1, () => setLoggingEnabled(true));
   useModuleStartCallback(2, () => setLoggingEnabled(true));
@@ -64,11 +66,36 @@ export default function DASRecording(): JSX.Element {
       <Button
         className="ml-2"
         variant="outline-danger"
-        onClick={stopRecording}
+        onClick={openConfirmModal}
         disabled={!loggingEnabled}
       >
         Stop
       </Button>
+
+      <Modal show={showConfirmModal} onHide={closeConfirmModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Are you Sure?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Stopping the logger will cause messy CSV files!</Modal.Body>
+        <Modal.Footer>
+          <Button
+            className="align-left"
+            variant="outline-danger"
+            onClick={closeConfirmModal}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="outline-success"
+            onClick={() => {
+              closeConfirmModal();
+              stopRecording();
+            }}
+          >
+            Yes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
