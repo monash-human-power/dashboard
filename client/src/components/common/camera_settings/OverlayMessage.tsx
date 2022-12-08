10d151e76371ab06d0cc655a3e05720505bef5a8
BoneFiend <mitchelldenardis@gmail.com>
2022-12-08 18:10:30 +1100

    Local message history
    Implemented an MVP local overlay message history in camera system. Formatting should work fine on mobile. Limitations include losing everything when changing pages.

--- client/src/components/common/camera_settings/OverlayMessage.tsx
+++ client/src/components/common/camera_settings/OverlayMessage.tsx
@@ -1,9 +1,16 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Button, Card, FormControl, InputGroup } from 'react-bootstrap';
+import styles from 'components/common/camera_settings/OverlayMessage.module.css';
 
 export interface OverlayMessageProps {
   sendMessage: (message: string) => void;
 }
+
+type Message = {
+  text: string;
+  time: Date;
+  elapsed: string; // "5 seconds" or something
+};
 
 /**
  * Text field for sending messages to the rider via the active overlay.
@@ -14,12 +21,20 @@ export interface OverlayMessageProps {
 export default function OverlayMessage({ sendMessage }: OverlayMessageProps) {
   const [message, setMessage] = useState('');
 
+  const [messageHistory, setMessageHistory] = useState<Message[]>([]);
+
   const handleMessageChange = useCallback(
     (event) => setMessage(event.target.value),
     [setMessage],
   );
 
   const handleMessage = useCallback(() => {
+    const newMessage: Message = {
+      text: message,
+      time: new Date(),
+      elapsed: 'now',
+    };
+    setMessageHistory((x) => [newMessage, ...x]);
     sendMessage(message);
     setMessage('');
   }, [message, setMessage, sendMessage]);
@@ -30,6 +45,37 @@ export default function OverlayMessage({ sendMessage }: OverlayMessageProps) {
     },
     [handleMessage],
   );
+
+  const updateElapsed = useCallback(() => {
+    const newMessageHistory = Object.create(messageHistory); // Deep copied
+    for (let m = 0; m < newMessageHistory.length; m += 1) {
+      let time =
+        (new Date().getTime() - newMessageHistory[m].time.getTime()) / 1000; // Elapsed seconds
+      // Finding best unit of time to use
+      let unit: string;
+      if (time < 60) {
+        unit = ' sec';
+      } else if (time < 60 * 60) {
+        unit = ' min';
+        time /= 60;
+      } else if (time < 60 * 60 * 24) {
+        unit = ' hour';
+        time /= 60 * 60;
+      } else {
+        unit = ' day';
+        time /= 60 * 60 * 24;
+      }
+      unit += time >= 2 ? 's' : '';
+      newMessageHistory[m].elapsed = Math.floor(time).toString().concat(unit); // Updates dummy variable
+    }
+    setMessageHistory(newMessageHistory);
+  }, [messageHistory, setMessageHistory]);
+
+  useEffect(() => {
+    // Updates elapsed times in messageHistory every second
+    const interval = setInterval(updateElapsed, 1000);
+    return () => clearInterval(interval);
+  }, [updateElapsed]);
 
   return (
     <Card>
@@ -48,6 +94,28 @@ export default function OverlayMessage({ sendMessage }: OverlayMessageProps) {
             </Button>
           </InputGroup.Append>
         </InputGroup>
+        {messageHistory.length > 0 && (
+          <table className={styles.table} id="messageTable">
+            <thead>
+              <tr>
+                <th>Message</th>
+                <th>Time</th>
+                <th className={styles.table_time}>Elapsed time</th>
+              </tr>
+            </thead>
+            <tbody>
+              {messageHistory.map(({ text, elapsed, time }) => (
+                <tr>
+                  <td className={styles.table_message}>{text}</td>
+                  <td className={styles.table_time}>
+                    {time.toLocaleTimeString('en-AU')}
+                  </td>
+                  <td>{elapsed}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </Card.Body>
     </Card>
   );
