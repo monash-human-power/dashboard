65a1c1e0ef47da479575e602b190a555dc2cd6f6
Arosh Heenkenda <ahee0011@student.monash.edu>
2024-03-28 15:47:26 +1100

    Continue bulding the base for trike page.

--- client/src/router/trike.ts
+++ client/src/router/trike.ts
@@ -1,5 +1,14 @@
+import React from 'react';
 import { RouteInfo } from 'types/route';
+import DashboardView from 'views/trike/DashboardView';
 
-const routes: RouteInfo[] = [];
+const routes: RouteInfo[] = [
+  {
+    name: 'Dashboard',
+    path: '/trike/',
+    exact: true,
+    component: (DashboardView as unknown) as React.Component,
+  },
+];
 
 export default routes;
---
+++ client/src/views/trike/DashboardView.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import styles from './DashboardView.module.css';
+
+/**
+ * Dashboard view component
+ *
+ * @returns Component
+ */
+export default function DashboardView(): JSX.Element {
+  return (
+    <Row className="styles.contentContainer">
+      <label> Help Me test this</label>
+    </Row>
+  );
+}
